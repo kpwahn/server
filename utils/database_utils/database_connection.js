20629a46958313effc2f31923d55ee1011feb815
Kendall Wahnschaffe <kpwahnschaffe@gmail.com>
2017-07-12 09:12:25 -0500

    initial commit

--- utils/database_utils/database_connection.js
+++ utils/database_utils/database_connection.js
@@ -1,6 +1,5 @@
 let mysql = require('mysql');
 
-// TODO export credentials to one place, and pull from server!!
 let connection = mysql.createPool({
     host     : 'localhost',
     user     : '',
