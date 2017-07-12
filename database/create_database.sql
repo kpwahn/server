557297c946a929c454267680d7d7e42399ea8112
Kendall Wahnschaffe <kpwahnschaffe@gmail.com>
2017-07-12 09:13:40 -0500

    initial commit

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,23 +1,9 @@
-CREATE DATABASE IF NOT EXISTS open_source_dance;
+CREATE DATABASE IF NOT EXISTS express_api_boilerplate;
 
 DROP TABLE user_table;
-DROP TABLE video_table;
-DROP TABLE user_video_table;
 
 CREATE TABLE user_table (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email varchar(255) NOT NULL,
     password varchar(255) NOT NULL
-);
-
-CREATE TABLE video_table (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    url varchar(255) NOT NULL
-);
-
-CREATE TABLE user_video_table (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id int REFERENCES user_table(id),
-    video_id int REFERENCES video_table(id)
 );
